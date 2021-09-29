45b6d9e5754a2df71457490085f14ddc4a92154c
SevenTrio <dimakrikunov12@gmail.com>
2021-09-29 22:11:38 +0300

    Added game and game over screens

--- App.js
+++ App.js
@@ -1,15 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 
 import Header from "./components/Header";
+import GameScreen from "./screens/GameScreen";
+import GameOverScreen from "./screens/GameOverScreen";
 import StartGameScreen from "./screens/StartGameScreen";
 
-export default function App() {
+const App = () => {
+    const [userNumber, setUserNumber] = useState();
+    const [guessRounds, setGuessRounds] = useState(0);
+    
+    const configureNewGame = () => {
+        setGuessRounds(0);
+        setUserNumber(null);
+    }
+
+    const startGameHandler = (selectedNumber) => {
+        setUserNumber(selectedNumber);
+        setGuessRounds(0);
+    };
+
+    const gameOverHandler = (numOfRounds) => {
+        setGuessRounds(numOfRounds);
+    }
+
+    let content = <StartGameScreen onStartGame={startGameHandler}/>;
+
+    if (userNumber && guessRounds <= 0) {
+        content = (
+            <GameScreen userChoice={userNumber} onGameOver={gameOverHandler}/>
+        );
+    } else if (guessRounds > 0) {
+        content = (
+            <GameOverScreen
+                roundsNumber={guessRounds}
+                userNumber={userNumber}
+                onRestart={configureNewGame}
+            />
+        );
+    }
+
     return (
         <View style={styles.screen}>
             <Header title="Guess a Number"/>
-            <StartGameScreen/>
+            {content}
             <StatusBar style="auto"/>
         </View>
     );
@@ -20,3 +55,5 @@ const styles = StyleSheet.create({
         flex: 1
     }
 });
+
+export default App;
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -13,7 +13,7 @@ import Input from "../components/Input";
 import NumberContainer from "../components/NumberContainer";
 import colors from "../constants/colors";
 
-const StartGameScreen = () => {
+const StartGameScreen = ({ onStartGame }) => {
     const [enteredValue, setEnteredValue] = useState('');
     const [confirmed, setConfirmed] = useState(false);
     const [selectedNumber, setSelectedNumber] = useState()
@@ -59,7 +59,9 @@ const StartGameScreen = () => {
             <Card style={styles.summaryContainer}>
                 <Text>You selected</Text>
                 <NumberContainer>{selectedNumber}</NumberContainer>
-                <Button title="START GAME"/>
+                <Button title="START GAME"
+                        onPress={() => onStartGame(selectedNumber)}
+                />
             </Card>
         );
     }
